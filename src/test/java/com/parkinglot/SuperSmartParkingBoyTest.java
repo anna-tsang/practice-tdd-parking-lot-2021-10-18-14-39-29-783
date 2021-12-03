9cf0c4c8e1247fc126abf20596f6467a39de9f9a
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-03 20:09:42 +0800

    [parkingLot-story5][Anna] feat: should_return_second_parking_lot_when_park_car_given_second_parking_lot_has_larger_empty_position_rate + implementation

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SuperSmartParkingBoyTest {
     private final String UNRECOGNISED_PARKING_TICKET = "Unrecognised parking ticket.";
     private final String NO_AVAILABLE_POSITION = "No available position.";
@@ -27,5 +30,21 @@ public class SuperSmartParkingBoyTest {
         //then
         assertEquals(1, parkingLotA.getAvailablePosition());
         assertEquals(2, parkingLotB.getAvailablePosition());
+    }
+
+    //case2
+    @Test
+    void should_return_second_parking_lot_when_park_car_given_second_parking_lot_has_larger_empty_position_rate() {
+        //given
+        ParkingLot parkingLotA = new ParkingLot(5);
+        ParkingLot parkingLotB = new ParkingLot(10);
+        SuperSmartParkingBoy parkingBoy = new SuperSmartParkingBoy(Arrays.asList(parkingLotA, parkingLotB));
+        Car car = new Car();
+        //when
+        Ticket ticket = parkingBoy.park(car);
+
+        //then
+        assertTrue(parkingLotB.ticketCarMap.containsValue(car));
+        assertFalse(parkingLotA.ticketCarMap.containsValue(car));
     }
 }
