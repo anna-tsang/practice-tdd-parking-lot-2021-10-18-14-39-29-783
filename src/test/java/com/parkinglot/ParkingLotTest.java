961f8ab144b8394055041c220001eca11e96a117
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-03 14:10:37 +0800

    [parkingLot][Anna]feat: should_return_nothing_when_park_car_given_full_parking_lot_and_car

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -3,16 +3,28 @@ package com.parkinglot;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ParkingLotTest {
     @Test
     void should_return_ticket_when_park_car_given_parking_lot_and_car() {
         //given
-        ParkingLot parkingLot = new ParkingLot();
+        ParkingLot parkingLot = new ParkingLot(10);
         Car car = new Car();
         //when
         Ticket ticket = parkingLot.parkCar(car);
         //then
         assertNotNull(ticket);
+    }
+
+    @Test
+    void should_return_nothing_when_park_car_given_full_parking_lot_and_car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot(10);
+        Car car = new Car();
+        //when
+        Ticket ticket = parkingLot.parkCar(car);
+        //then
+        assertNull(ticket);
     }
 }
