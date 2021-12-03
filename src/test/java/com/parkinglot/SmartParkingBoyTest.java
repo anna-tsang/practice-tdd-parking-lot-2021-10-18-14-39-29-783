b8ff59ee2564c3e506dfdd90dbd612d121eb0c0f
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-03 19:33:10 +0800

    [parkingLot-story4][Anna] feat: should_return_first_parking_lot_when_park_car_given_same_number_of_empty_position + implementation

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -11,4 +11,19 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class SmartParkingBoyTest {
     private final String UNRECOGNISED_PARKING_TICKET = "Unrecognised parking ticket.";
     private final String NO_AVAILABLE_POSITION = "No available position.";
+
+    @Test
+    void should_return_first_parking_lot_when_park_car_given_same_number_of_empty_position() {
+        //given
+        ParkingLot parkingLotA = new ParkingLot(2);
+        ParkingLot parkingLotB = new ParkingLot(2);
+        List<ParkingLot> parkingLotList = Arrays.asList(parkingLotA,parkingLotB);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLotList);
+        //when
+        Ticket ticket = smartParkingBoy.park(new Car());
+
+        //then
+        assertEquals(1, parkingLotA.getAvailablePosition());
+        assertEquals(2, parkingLotB.getAvailablePosition());
+    }
 }
