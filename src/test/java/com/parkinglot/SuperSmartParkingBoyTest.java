b83ab0084045e197542abe8817731161e856f70e
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-03 20:12:05 +0800

    [parkingLot-story5][Anna] feat: should_return_error_message_when_fetch_car_given_used_ticket + implementation

--- src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperSmartParkingBoyTest.java
@@ -92,4 +92,27 @@ public class SuperSmartParkingBoyTest {
         //then
         assertEquals(UNRECOGNISED_PARKING_TICKET, unrecognisedParkingTicket.getMessage());
     }
+
+    //case5
+    @Test
+    void should_return_error_message_when_fetch_car_given_used_ticket() {
+        //given
+        ParkingLot parkingLotA = new ParkingLot();
+        ParkingLot parkingLotB = new ParkingLot();
+        Car carA = new Car();
+        Car carB = new Car();
+        Ticket ticketA = parkingLotA.park(carA);
+        Ticket ticketB = parkingLotB.park(carB);
+        Ticket wrongTicket = new Ticket();
+        List<ParkingLot> parkingLotList = Arrays.asList(parkingLotA,parkingLotB);
+        SuperSmartParkingBoy parkingBoy = new SuperSmartParkingBoy(parkingLotList);
+
+        //when
+        UnrecognisedParkingTicket unrecognisedParkingTicket = assertThrows(UnrecognisedParkingTicket.class, () -> {
+            Car fetchCar = parkingBoy.fetch(wrongTicket);
+        });
+
+        //then
+        assertEquals(UNRECOGNISED_PARKING_TICKET, unrecognisedParkingTicket.getMessage());
+    }
 }
