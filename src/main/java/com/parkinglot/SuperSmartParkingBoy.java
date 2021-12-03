bc9e6ac3ef0d5d64a479dad3af3b19c51e10288e
ANNA TSANG <TSANGAN@OOCL.COM>
2021-12-03 20:15:08 +0800

    [parkingLot-story5][Anna] feat: remove spacing

--- src/main/java/com/parkinglot/SuperSmartParkingBoy.java
+++ src/main/java/com/parkinglot/SuperSmartParkingBoy.java
@@ -3,8 +3,6 @@ package com.parkinglot;
 import java.util.List;
 
 public class SuperSmartParkingBoy extends SmartParkingBoy {
-    private final String UNRECOGNISED_PARKING_TICKET = "Unrecognised parking ticket.";
-    private final String NO_AVAILABLE_POSITION = "No available position.";
 
     public SuperSmartParkingBoy(List<ParkingLot> parkingLotList) {
         super(parkingLotList);
